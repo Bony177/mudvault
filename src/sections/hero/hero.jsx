f4f85a854433419ad8a12ebc9f2ed1856a8a91b8
Bony177 <bonybabu61@gmail.com>
2026-08-12 01:55:40 +0530

    Initial website setup

--- src/sections/hero/hero.jsx
+++ src/sections/hero/hero.jsx
@@ -1,10 +1,17 @@
 import "./Hero.css";
 import background from "../../assets/background.webp";
+import mudvaultTitle from "../../assets/mudvault.png";
 
 function Hero() {
   return (
     <section className="hero">
-      <img className="hero-background" src={background} alt="Hero background" />
+      {/* Background */}
+      <img className="hero-background" src={background} alt="" />
+
+      {/* MUDVAULT title */}
+      <div className="hero-content">
+        <img className="hero-title" src={mudvaultTitle} alt="MUDVAULT" />
+      </div>
     </section>
   );
 }
